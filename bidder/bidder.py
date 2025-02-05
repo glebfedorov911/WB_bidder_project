b555afcb491e9d09e15c479e6799d997d7aa9477
Gleb <fedorov22134@gmail.com>
2025-02-05 23:29:28 +0300

    edit __init__

--- bidder/bidder.py
+++ bidder/bidder.py
@@ -31,20 +31,28 @@ class Bidder:
     def __init__(self, bidder_data: BidderData, cpm_change: CPMChangeSchema, calculator: Calculator):
         super().__init__()
         
-        self.__dict__.update(vars(bidder_data))
-        self.__dict__.update(vars(cpm_change))
-        
+        self.max_cpm_campaign = bidder_data.max_cpm_campaign
+        self.min_cpm_campaign = bidder_data.min_cpm_campaign
+        self.wish_place_in_top = bidder_data.wish_place_in_top
+        self.type_work_bidder = bidder_data.type_work_bidder
+        self.step = bidder_data.step
+
+        self.advertId = cpm_change.bidder_data
+        self.type = self._get_wb_code_for_type(type_campaign=cpm_change.type)
+        self.cpm = cpm_change.cpm
+        self.param = cpm_change.param
+        self.instrument = cpm_change.instrument
+
         self.calculator = calculator
 
-        self.type = self._get_wb_code_for_type(type_campaign=cpm_change.type)
-        self.min_cpm_campaign = self._get_min_cpm_campaign()
+        self.start_cpm = self._get_start_cpm_campaign()
         
     def _get_wb_code_for_type(self, type_campaign: TypeCampaign) -> int:
         if not hasattr(type_campaign, "wb_code"):
             raise ValueError(INVALID_CAMPAIGN_TYPE_WB_CODE_MISSING)
         return type_campaign.wb_code
 
-    def _get_min_cpm_campaign(self) -> int:
+    def _get_start_cpm_campaign(self) -> int:
         return self.calculator.calculate_start_cpm()
 
     def up_cpm(self) -> None:
